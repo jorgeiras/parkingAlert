31937f1336c5d77f920b7412536223e80e1bc8a2
jorgeiras <jorgeiras.alonso@hotmail.com>
2022-07-18 21:16:53 +0200

    Cambiada la funcionalidad de coger la posición mediante GPS para que sea más rápido.

--- app/src/main/java/com/example/parkingalert/RecentParkingsFragment.java
+++ app/src/main/java/com/example/parkingalert/RecentParkingsFragment.java
@@ -2,29 +2,42 @@ package com.example.parkingalert;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -58,6 +71,7 @@ public class RecentParkingsFragment extends Fragment {
     List<ParkingSpace> parkingSpaceList;
     parkAdapter parkAdapterObject;
     private SwipeRefreshLayout swipeRefreshLayout;
+    LocationRequest locationRequest;
 
 
     // TODO: Rename parameter arguments, choose names that match
@@ -100,6 +114,7 @@ public class RecentParkingsFragment extends Fragment {
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.S)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -109,7 +124,7 @@ public class RecentParkingsFragment extends Fragment {
 
         swipeRefreshLayout = (SwipeRefreshLayout) v.findViewById(R.id.swipeRefreshRecent);
 
-
+        getDeviceLocation();
 
         db = FirebaseFirestore.getInstance();
         recyclerView = v.findViewById(R.id.recyclerRecent);
@@ -118,12 +133,13 @@ public class RecentParkingsFragment extends Fragment {
         parkAdapterObject = new parkAdapter(getActivity(), parkingSpaceList);
         recyclerView.setAdapter(parkAdapterObject);
 
-        getDeviceLocation();
+
 
 
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
+                getDeviceLocation();
                 recyclerView.setAdapter(parkAdapterObject);
                 parkingSpaceList.clear();
                 db.collection("Parkings").orderBy("timeStamp", Query.Direction.DESCENDING).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -137,12 +153,12 @@ public class RecentParkingsFragment extends Fragment {
                                 double lat = 0.0144927536231884;
                                 double lon = 0.0181818181818182;
                                 double distance = 2;
-                                double lowLat = latitude- (distance * lat);
+                                double lowLat = latitude - (distance * lat);
                                 double highLat = latitude + (distance * lat);
                                 double lowLong = longitude - (distance * lon);
                                 double highLong = longitude + (distance * lon);
 
-                                if( p.getLatitude() > lowLat && p.getLatitude() < highLat && p.getLongitude() > lowLong && p.getLongitude() < highLong ){
+                                if (p.getLatitude() > lowLat && p.getLatitude() < highLat && p.getLongitude() > lowLong && p.getLongitude() < highLong) {
 
                                     parkingSpaceList.add(p);
                                 }
@@ -163,10 +179,75 @@ public class RecentParkingsFragment extends Fragment {
     }
 
 
+    @RequiresApi(api = Build.VERSION_CODES.S)
     private void getDeviceLocation() {
 
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(2000);
+
+        LocationSettingsRequest.Builder build = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+        build.setAlwaysShow(true);
+
+        Task<LocationSettingsResponse> response = LocationServices.getSettingsClient(getActivity().getApplicationContext()).checkLocationSettings(build.build());
+
+        response.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+                try {
+                    LocationSettingsResponse res = task.getResult(ApiException.class);
+                    Toast.makeText(getActivity(), "GPS ya activado", Toast.LENGTH_LONG).show();
+                } catch (ApiException e) {
+
+                    switch (e.getStatusCode()) {
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+
+                            try {
+                                ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                                resolvableApiException.startResolutionForResult(getActivity(), 2);
+                            } catch (IntentSender.SendIntentException exception) {
+                                exception.printStackTrace();
+                            }
+                            break;
+
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            break;
+                    }
+                }
+            }
+        });
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationServices.getFusedLocationProviderClient(getActivity()).requestLocationUpdates(locationRequest, new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                LocationServices.getFusedLocationProviderClient(getActivity()).removeLocationUpdates(this);
+
+                if(locationResult!=null && locationResult.getLocations().size() > 0){
+                    latitude = locationResult.getLocations().get(locationResult.getLocations().size()-1).getLatitude();
+                    longitude = locationResult.getLocations().get(locationResult.getLocations().size()-1).getLongitude();
+                }
+            }
+        }, Looper.getMainLooper());
+
+
+        /*
         try {
-            fusedLoc = LocationServices.getFusedLocationProviderClient(getContext());
+            if(fusedLoc==null){
+                fusedLoc = LocationServices.getFusedLocationProviderClient(getContext());
+            }
+
             Task<Location> locationResult = fusedLoc.getLastLocation();
             locationResult.addOnSuccessListener( new OnSuccessListener<Location>() {
                 @Override
@@ -185,14 +266,24 @@ public class RecentParkingsFragment extends Fragment {
 
         } catch (SecurityException e)  {
             Log.e("Exception: %s", e.getMessage(), e);
-        }
+        }*/
     }
 
 
+
+
+
+
+
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.S)
     @Override
     public void onResume() {
 
         super.onResume();
+        getDeviceLocation();
         recyclerView.setAdapter(parkAdapterObject);
         parkingSpaceList.clear();
         db.collection("Parkings").orderBy("timeStamp", Query.Direction.DESCENDING).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
